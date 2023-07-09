aa35b9b35233f936648d4305edac073bf3a3aeb8
BLENDER69 <138211895+BLENDER69@users.noreply.github.com>
2023-07-10 03:17:44 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,16 +21,16 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['919074692450'] //ur owner number
-global.ownernomer = "919074692450" //ur owner number2
-global.ownername = "ABHISHEK SURESH"//ur owner name
+global.owner = ['92107062695'] //ur owner number
+global.ownernomer = "923456194602" //ur owner number2
+global.ownername = "ABDUL HASEEB"//ur owner name
 global.ytname = "YT: Comedy Melody CH" //ur yt chanel name
 global.socialm = "GitHub: AbhishekSuresh2" //ur github or insta name
 global.location = "India, Idukki, Kerala" //ur location
 
 //new
-global.botname = "ABHISHEK-SER"
-global.ownernumber = '919074692450'
+global.botname = "BLENDER-BOT"
+global.ownernumber = '923456194602'
 global.ownername = 'ABHISHEK SURESH'
 global.ownerNumber = ["919074692450@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@comedymelodych8468"
@@ -39,8 +39,8 @@ global.wagc = "https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD"
 global.themeemoji = '🎐'
 global.wm = "ABHISHEK-SER Inc."
 global.botscript = 'https://github.com/AbhishekSuresh2/ABHISHEK-SER' //script link
-global.packname = "ABHISHEK-SER👻ABHISHEK SURESH"
-global.author = "ABHISHEK SURESH\n\n919074692450"
+global.packname = "BLENDER"
+global.author = "ABDUL HASEEB"
 global.creator = "919074692450@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
